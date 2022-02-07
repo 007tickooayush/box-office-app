e331d9d608090f153162908b6432a8a5f112ad06
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-07 10:56:19 +0530

    Fixed Error ERROR in Failed to load parser 'babel-eslint' declared in 'src\.eslintrc': Cannot find module 'babel-eslint'

--- README.md
+++ README.md
@@ -20,3 +20,8 @@ install-peerdeps --dev eslint-config-airbnb
 ```
 npm install --save-dev eslint-config-prettier
 ```
+
+## : `Installing react router v5` 
+```
+npm install react-router-dom@5.2.0 react-router@5.2.0
+```
--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path="/"> 
+        This is home page
+        </Route>
+      </Switch>
       hi
     </div>
   );
