6dcaa7df3a51fc6f271b88c40218a4623a53f059
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-13 11:54:06 +0530

    created input field and embedded Fetch functioning from TV Maze Api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 function Home() {
-  return <MainPageLayout>this is home component</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+    // console.log(ev.target.value);
+  };
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => console.log(result));
+  };
+
+  const onKeyDown = (ev) =>{
+    // ** map the keycode to get results with enter key as well **
+    if(ev.keyCode === 13){
+      onSearch()
+    }
+    // console.log(ev.keyCode);
+
+  };
+
+  return (
+    <MainPageLayout>
+      {/* associated the input State with input field by using `value = {input}` */}
+      <input onChange={onInputChange} value={input} onKeyDown={onKeyDown}/>
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 }
 
 export default Home;
