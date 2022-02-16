8ec10718107dd9659a8136f7a3cfa51316c90976
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-16 12:40:54 +0530

    replaced useState with useReducer in Show component

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,13 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
 
 function Show() {
   // destructuring and fetching only the 'id' param
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState('');
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  //   const [show, setShow] = useState(null);
+  //   const [isLoading, setIsLoading] = useState(true);
+  //   const [error, setError] = useState('');
 
   // console.log('params',params);
   useEffect(() => {
@@ -16,14 +42,20 @@ function Show() {
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
         if (isMounted) {
-          setShow(results);
-          setIsLoading(false);
+          // using dispatch function instead of multiple state updates
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
+
+          //   setShow(results);
+          //   setIsLoading(false);
         }
       })
       .catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setIsLoading(false);
+          // using dispatch functoin to update error
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+
+          //   setError(err.message);
+          //   setIsLoading(false);
         }
       });
 
