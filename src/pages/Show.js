b4899e1633aa3c2e827692ac8202904edb016636
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-16 12:25:02 +0530

    added loading functionality and error handling

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,24 +1,48 @@
-import React,{useState,useEffect} from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
 function Show() {
+  // destructuring and fetching only the 'id' param
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
 
-    // destructuring and fetching only the 'id' param
-    const {id} = useParams();
-    const [show,setShow] = useState(null);
-    // console.log('params',params);
-    useEffect(() =>{
-        
-        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then( results =>{
-            setShow(results);
-        })
+  // console.log('params',params);
+  useEffect(() => {
+    let isMounted = true;
 
-    },[id]);
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
 
-    console.log('show', show);
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
 
-    return <div>Show page</div>;
+  console.log('show', show);
+
+  if (isLoading) {
+    return <div> Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div> Error occured: {error} </div>;
+  }
+
+  return <div>Show page</div>;
 }
 
 export default Show;
